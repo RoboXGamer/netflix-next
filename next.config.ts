68fb9a86ebbe366f30c606f7283104fd9115dfaf
RoboXGamer <roboxgamer52@gmail.com>
2025-11-23 19:12:37 +0530

    Implemented MVP features with Hero and Trending Now Section on the index route

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  env: {
+    TMDB_AUTH_TOKEN: process.env.TMDB_AUTH_TOKEN,
+  },
 };
 
 export default nextConfig;
